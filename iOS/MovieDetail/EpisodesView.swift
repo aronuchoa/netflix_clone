330451124cf0c298eef5cc6e2a062f021e1630aa
Aron Uchoa <aron.uchoa@hotmail.com>
2020-12-28 18:03:00 -0300

    add custom tab for movie detail episodeView

--- iOS/MovieDetail/EpisodesView.swift
+++ iOS/MovieDetail/EpisodesView.swift
@@ -10,13 +10,74 @@ import SwiftUI
 struct EpisodesView: View {
     var episodes: [Episode]
     
+    @Binding var showSeasonPicker: Bool
+    @Binding var selectedSeason: Int
+    
+    func getEpisodes(forSeason season: Int) -> [Episode] {
+        return episodes.filter({ $0.season == season })
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 14) {
+            // Season Picker
+            HStack {
+                Button(action: {
+                    showSeasonPicker = true
+                }, label: {
+                    Group {
+                        Text("Season \(selectedSeason)")
+                        Image(systemName: "chevron.down")
+                    }
+                    .font(.system(size: 16))
+                })
+                
+                Spacer()
+            }
+            
+            // Episodes List
+            
+            ForEach(getEpisodes(forSeason: selectedSeason)) { episode in
+                VStack(alignment: .leading) {
+                    HStack {
+                        VideoPreviewImage(imageURL: episode.thumbnailURL, videoURL: episode.videoURL)
+                            .frame(width: 120, height: 70)
+                            .clipped()
+                        VStack(alignment: .leading) {
+                            Text("\(episode.episodeNumber). \(episode.name)")
+                                .font(.system(size: 16))
+                            Text("\(episode.length)m")
+                                .font(.system(size: 12))
+                                .foregroundColor(.gray)
+                        }
+                        
+                        Spacer()
+                        
+                        Image(systemName: "arrow.down.to.line.alt")
+                            .font(.system(size: 20))
+                    }
+                    
+                    // Description
+                    Text(episode.description)
+                        .font(.system(size: 13))
+                        .lineLimit(3)
+                }
+                .padding(.bottom, 20)
+            }
+            
+            Spacer()
+        }
+        .foregroundColor(.white)
+        .padding(.horizontal, 20)
     }
 }
 
 struct EpisodesView_Previews: PreviewProvider {
     static var previews: some View {
-        EpisodesView(episodes: [episode1, episode2, episode3])
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            EpisodesView(episodes: allExampleEpisodes, showSeasonPicker: .constant(false), selectedSeason: .constant(1))
+        }
     }
 }
